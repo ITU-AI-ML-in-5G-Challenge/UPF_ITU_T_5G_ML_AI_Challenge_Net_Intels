234547afcfd2a53fa838ef225857dcc6d227ad8a
K Venkat Ramnan <venkatramnank@gmail.com>
2020-10-23 13:05:01 +0530

    Add files via upload

---
+++ Preprocessing/Training/Combiner.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+import glob
+
+path = "C:\\Users\\venka\\Desktop\\5g_ITU\\Combined\\"
+os.chdir(path)
+results = pd.DataFrame()
+
+for counter, current_file in enumerate(glob.glob("*.csv")):
+    namedf = pd.read_csv(current_file, header=None, sep="|")
+    print(namedf)
+    results = pd.concat([results, namedf])
+
+results.to_csv('Combined.csv', index=None, header=None, sep="|")
---
+++ Preprocessing/Training/delete_empty.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def delete_empty_rows(file_path, new_file_path):
+    data = pd.read_csv(file_path, skip_blank_lines=True)
+    data.dropna(how="all", inplace=True)
+    data.to_csv(new_file_path, header=True)
+
+delete_empty_rows('Combined_rssi_corrected_proper_sinr.csv', 'Combined_rssi_corrected_proper_sinr_row_fixed.csv')
