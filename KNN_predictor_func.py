8283027d4dcc8cc9a5dd5787805b08e4894f5224
K Venkat Ramnan <venkatramnank@gmail.com>
2020-10-24 13:25:44 +0530

    Update KNN_predictor_func.py

--- KNN_predictor_func.py
+++ KNN_predictor_func.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsRegressor
 import re
