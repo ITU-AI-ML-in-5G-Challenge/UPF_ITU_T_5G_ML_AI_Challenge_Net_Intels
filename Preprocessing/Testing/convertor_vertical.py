a26072dc970712b9da81991c11f1b9a81f26a754
K Venkat Ramnan <venkatramnank@gmail.com>
2020-10-24 15:55:48 +0530

    Update convertor_vertical.py

--- Preprocessing/Testing/convertor_vertical.py
+++ Preprocessing/Testing/convertor_vertical.py
@@ -1,3 +1,4 @@
+# This code is used to tranpose from rows to columns
 import pandas as pd
 import glob
 import re
@@ -14,11 +15,3 @@ for file in glob.glob(pathname+'sinr_*.csv'):
 	file_name=re.sub('.csv','',file)
 	df2=df.transpose()
 	df2.to_csv(file_name+'new.csv')
-
-'''
-for f1,f2 in zip(glob.glob(pathname+'rssi_*new.csv'),(pathname+'input_*.csv')):
-	df1=pd.read_csv(f1)
-	df2=pd.read_csv(f2)
-	file_name_2=re.sub('.csv','',f2)
-	df3=pd.concat(df1,df2)
-	df3.to_csv(file_name_2+'new.csv')'''
