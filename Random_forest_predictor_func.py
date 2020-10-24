96f740466d045bf20b0dc39400732a40fca889ab
K Venkat Ramnan <venkatramnank@gmail.com>
2020-10-24 13:26:06 +0530

    Update Random_forest_predictor_func.py

--- Random_forest_predictor_func.py
+++ Random_forest_predictor_func.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 import re
